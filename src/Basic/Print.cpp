945844a2de807d6609fb51489681de19632d38a6
Zxilly <zxilly@outlook.com>
2025-07-31 13:30:57 +0800

    feat: disable color on non-interactive tty

--- src/Basic/Print.cpp
+++ src/Basic/Print.cpp
@@ -8,6 +8,9 @@
 
 #ifdef _WIN32
 #include <windows.h>
+#else
+#include <unistd.h>
+#include <cstdlib>
 #endif
 
 namespace Cangjie {
@@ -23,10 +26,27 @@ const int WINDOWS_10_VERSION_1511_BUILD_NUMBER = 10586;
 const int WINDOWS_10 = 10;
 ColorSingleton::ColorSingleton()
 {
+    if (std::getenv("NO_COLOR") != nullptr) {
+        return;
+    }
+
+    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    HANDLE hStderr = GetStdHandle(STD_ERROR_HANDLE);
+    if (hStdout == INVALID_HANDLE_VALUE || hStderr == INVALID_HANDLE_VALUE) {
+        return;
+    }
+    if (hStdout == nullptr || hStderr == nullptr) {
+        return;
+    }
+
     DWORD stdoutMode;
     DWORD stderrMode;
-    GetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), &stdoutMode);
-    GetConsoleMode(GetStdHandle(STD_ERROR_HANDLE), &stderrMode);
+    if (!GetConsoleMode(hStdout, &stdoutMode)) {
+        return;
+    }
+    if (!GetConsoleMode(hStderr, &stderrMode)) {
+        return;
+    }
 
     // store initial console mode
     initialStdoutMode = stdoutMode;
@@ -38,8 +58,12 @@ ColorSingleton::ColorSingleton()
         // current Windows os version is or newer than Windows10 (version 1511)
         stdoutMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
         stderrMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-        SetConsoleMode(GetStdHandle(STD_OUTPUT_HANDLE), stdoutMode);
-        SetConsoleMode(GetStdHandle(STD_ERROR_HANDLE), stderrMode);
+        if (!SetConsoleMode(hStdout, stdoutMode)) {
+            return;
+        }
+        if (!SetConsoleMode(hStderr, stderrMode)) {
+            return;
+        }
 
         ANSI_COLOR_RESET = "\x1b[0m";
         ANSI_COLOR_BRIGHT = "\x1b[1m";
@@ -63,20 +87,40 @@ ColorSingleton::~ColorSingleton()
     SetConsoleMode(GetStdHandle(STD_ERROR_HANDLE), initialStderrMode);
 }
 #else
+static bool SupportsColor()
+{
+    if (std::getenv("NO_COLOR") != nullptr) {
+        return false;
+    }
+    
+    if (!isatty(STDOUT_FILENO)) {
+        return false;
+    }
+    
+    const char* term = std::getenv("TERM");
+    if (term == nullptr || std::string(term) == "dumb") {
+        return false;
+    }
+    
+    return true;
+}
+
 ColorSingleton::ColorSingleton()
 {
-    ANSI_COLOR_RESET = "\x1b[0m";
-    ANSI_COLOR_BRIGHT = "\x1b[1m";
-    ANSI_COLOR_BLACK = "\x1b[30m";
-    ANSI_COLOR_RED = "\x1b[31m";
-    ANSI_COLOR_GREEN = "\x1b[32m";
-    ANSI_COLOR_YELLOW = "\x1b[33m";
-    ANSI_COLOR_BLUE = "\x1b[34m";
-    ANSI_COLOR_MAGENTA = "\x1b[35m";
-    ANSI_COLOR_CYAN = "\x1b[36m";
-    ANSI_COLOR_WHITE = "\x1b[37m";
+    if (SupportsColor()) {
+        ANSI_COLOR_RESET = "\x1b[0m";
+        ANSI_COLOR_BRIGHT = "\x1b[1m";
+        ANSI_COLOR_BLACK = "\x1b[30m";
+        ANSI_COLOR_RED = "\x1b[31m";
+        ANSI_COLOR_GREEN = "\x1b[32m";
+        ANSI_COLOR_YELLOW = "\x1b[33m";
+        ANSI_COLOR_BLUE = "\x1b[34m";
+        ANSI_COLOR_MAGENTA = "\x1b[35m";
+        ANSI_COLOR_CYAN = "\x1b[36m";
+        ANSI_COLOR_WHITE = "\x1b[37m";
 
-    ANSI_COLOR_WHITE_BACKGROUND_BLACK_FOREGROUND = "\x1b[30;47m";
+        ANSI_COLOR_WHITE_BACKGROUND_BLACK_FOREGROUND = "\x1b[30;47m";
+    }
 };
 
 ColorSingleton::~ColorSingleton(){};
