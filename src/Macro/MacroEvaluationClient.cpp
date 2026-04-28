fa3e68029173b34b426565f753f0def64c035dee
guan_jh <guan_jh_dut@163.com>
2026-04-28 21:01:41 +0800

    fix: fix lsp macro path fail
    Signed-off-by: guan_jh <guan_jh_dut@163.com>

--- src/Macro/MacroEvaluationClient.cpp
+++ src/Macro/MacroEvaluationClient.cpp
@@ -500,8 +500,9 @@ void MacroEvaluation::ExecMacroSrv(pid_t pid) const
 {
     // args
     std::vector<char*> cstrings;
+    // for linux: cjc 可执行文件与 LSPMacroServer不在一个目录, 需特殊处理
     std::string macSrvPath = FileUtil::JoinPath(
-        FileUtil::GetDirPath(ci->invocation.globalOptions.executablePath), MACRO_SRV_NAME);
+        FileUtil::GetDirPath(ci->invocation.globalOptions.executablePath), "../tools/bin/" + MACRO_SRV_NAME);
     std::string hRead = std::to_string(MacroProcMsger::GetInstance().pipefdP2C[0]);
     std::string hWrite = std::to_string(MacroProcMsger::GetInstance().pipefdC2P[1]);
     std::string enPara = enableParallelMacro ? "1" : "0";
@@ -645,8 +646,9 @@ bool CreateMacroMsgPipe()
 
 std::string GetMacroSrvCmd(bool enableParallelMacro, std::string& cjcPath)
 {
+    // for windows: cjc 可执行文件与 LSPMacroServer不在一个目录, 需特殊处理
     std::string cmdStr = FileUtil::JoinPath(
-        FileUtil::GetDirPath(cjcPath), MACRO_SRV_NAME + ".exe");
+        FileUtil::GetDirPath(cjcPath), "..\\tools\\bin\\" + MACRO_SRV_NAME + ".exe");
     const size_t buffLen = 20;
     char handlebuffer[buffLen];
     sprintf_s(handlebuffer, buffLen, "%d", MacroProcMsger::GetInstance().hChildRead);
