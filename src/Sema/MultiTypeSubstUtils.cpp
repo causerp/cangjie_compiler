3ff0c3da0e714645c5cc30f37529950362858862
softfault <givenyoung46@gmail.com>
2026-07-29 10:51:37 +0800

    fix: remove redundant null type filtering
    Signed-off-by: softfault <givenyoung46@gmail.com>

--- src/Sema/MultiTypeSubstUtils.cpp
+++ src/Sema/MultiTypeSubstUtils.cpp
@@ -61,9 +61,6 @@ SubstPack FilterUnusedMapping(const SubstPack& mapping, const std::set<Ptr<Ty>>&
     }
     // collect inst tyvars directly used
     for (auto ty : tys) {
-        if (!ty) {
-            continue;
-        }
         for (auto tvu : ty->GetGenericTyArgs(allu)) {
             reachable.emplace(StaticCast<TyVar*>(mapping.u2i.at(tvu)));
         }
@@ -103,9 +100,6 @@ MultiTypeSubst FilterUnusedMapping(const MultiTypeSubst& mapping, const std::set
     std::queue<Ptr<TyVar>> worklist;
     // collect tyvars directly used
     for (auto ty : tys) {
-        if (!ty) {
-            continue;
-        }
         reachable.merge(ty->GetGenericTyArgs(all));
     }
     for (auto tv : reachable) {
